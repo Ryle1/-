505ed40c88dec230b5342ac0582e89baa0589de2
Ryle1 <ks35679@naver.com>
2023-02-06 23:05:43 +0900

    Coding Practice

---
+++ 구구단/소스.c
@@ -0,0 +1,42 @@
+// 구구단
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, number, answer;
+
+	printf("구구단 : ");
+	scanf("%d", &number);
+
+	for (i = 1; i <= number; i++)
+	{
+		printf("%d단\n", i);
+		printf("----------\n");
+
+		for (j = 1; j <= 9; j++)
+		{
+			answer = i * j;
+			printf("%d * %d = %d\n", i, j, answer);
+		}
+		printf("----------\n");
+	}
+
+	return 0;
+}
+
+/*
+실행결과
+1단
+----------
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+1 x 4 = 4
+1 x 5 = 5
+1 x 6 = 6
+1 x 7 = 7
+1 x 8 = 8
+1 x 9 = 9
+----------
+*/
